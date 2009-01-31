4cb378c27af2d72f484c83a8b099f697c8b7950a
unknown <hangeles@.(none)>
2009-01-31 18:38:22 -0500

    first commit

---
+++ ATrack/ATrackInt.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Text;
+using ATrack.Library;
+
+namespace ATrack
+{
+    class Program
+    {
+        static string[] validArgs = new string[7] { "/F", "/L", "/N", "/C", "/D", "/Q", "/O" };
+        private static string wmiNameSpace = string.Empty;
+        private static string wmiClass = string.Empty;
+
+        static void Main(string[] args)
+        {
+
+            if ((args.Length == 0) || (args.Length > 4))
+            {
+                PrintHelp();
+                return;
+            }
+            else if (args.Length != 0)
+            {
+                VerifyArgs(args);
+            }
+
+        }
+
+        private static void PrintHelp()
+        {
+            string appName = Process.GetCurrentProcess().ProcessName;
+            StringBuilder s = new StringBuilder();
+            s.Append("\n" + appName + " ");
+            s.Append("[/F] <file> [/N] <namespace>  [/C] <class> [/O] <computer> <query> \n\t[/L] [/D]\n\n");
+            s.Append("   /F\t\tFile with list of computers to be queried.\n");
+            s.Append("     \t\tThe File must have one entry per line\n");
+            s.Append("     \t\tand must be followed by /Q and a query.\n");
+            s.Append("   /L\t\tList Available NameSpaces\n");
+            s.Append("   /N\t\tUse specified WMI NameSpace. If no NameSpace is given,\n");
+            s.Append("     \t\t'root\\CIMV2' is used by default.\n");
+            s.Append("   /C\t\tUse specified WMI Class.\n");
+            s.Append("   /D\t\tUse The default NameSpace 'root\\CIMV2' and the default Class\n");
+            s.Append("   \t\t'Win32_ComputerSystem' and display on screen\n");
+            s.Append("   /Q\t\tQuery WMI using WQL\n");
+            s.Append("   /O\t\tQuery using WQL and output to file. Needs a file name, a DNS\n");
+            s.Append("     \t\tresovable computer name or ip address and valid WQL query in\n");
+            s.Append("      \t\tquotes.\n");
+
+            Console.WriteLine("{0}", s);
+            Environment.Exit(0);
+            //Console.Read();
+        }
+
+        private static void UseDefaults()
+        {
+            wmiClass = "Win32_ComputerSystem";
+            wmiNameSpace = "root\\CIMV2";
+            //wmiClass = "__NAMESPACE";
+            //wmiNameSpace = "root";
+        }
+
+        private static void VerifyArgs(string[] args)
+        {
+            int i = 0;
+            /*foreach (string arg in args)
+            {
+                Console.WriteLine("Arg {0} : {1}", i, args.GetValue(i));
+                i++;
+            }
+             */
+          
+            if (args[0].Contains("/L"))
+            {
+                ATrackLib.GetCIMV2Classes();
+                Environment.Exit(0);
+            }
+            else if (args[0].Contains("/D"))
+            {
+                UseDefaults();
+                string compName = Environment.MachineName;
+                string fileName = "C:\\" + compName + ".txt";
+                ATrackLib.DisplayInfo(@"\\.\root\CIMV2:" + wmiClass + "='" + compName + "'");
+                Environment.Exit(1);
+            }
+            else if (args[0].Contains("/O"))
+            {
+                if (args.Length >= 3)
+                {
+                    try
+                    {
+                        Console.WriteLine("Doing stuff with /O arg\n");
+                        DoStub();
+                        //ATrackLib.QueryDataOutput(args[1].ToString(), args[2]);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("\nCheck your parameters.\n");
+                        Environment.Exit(1);
+                    }
+                }
+                else
+                {
+                    PrintHelp();
+                }
+
+            }
+            else if (args[0].Contains("/Q"))
+            {
+                try
+                {
+                    ATrackLib.QueryData(@"\\.\root\CIMV2:", args[1].ToString());
+                }
+                catch
+                {
+                    Console.WriteLine("\nCheck your query.\n");
+                    Environment.Exit(1);
+                }
+            }
+            else
+            {
+                PrintHelp();
+            }
+        }
+
+        private static void DoStub()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("Stub to Catch all unimplemneted methods");
+            return;
+        }
+
+    }
+
+}
---
+++ ATrackLib/ATrackLib.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management;
+using System.Collections;
+
+namespace ATrack.Library
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ATrackLib
+    {
+
+
+        /*public static Hashtable GetData(string location)
+        {
+            Hashtable list = new Hashtable();
+
+            ManagementObject mo = new ManagementObject(location);
+            foreach (PropertyData d in mo.Properties)
+            {
+                list.Add(d.Name, d.Value);
+            }
+            return list;
+        }
+        */
+
+        public static void QueryData(string location, string inquery)
+        {
+            
+            ConnectionOptions co = new ConnectionOptions();
+            ManagementScope scope = new ManagementScope(@"\\.\root\CIMV2", co);
+            ObjectQuery query = new ObjectQuery(inquery);
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+            ManagementObjectCollection collection = searcher.Get();
+
+            foreach (ManagementObject wmiobj in collection)
+            {
+                PropertyDataCollection searcherProperties = wmiobj.Properties;
+                Console.WriteLine("Class : {0}\n", wmiobj.ClassPath.ClassName);
+
+                foreach (PropertyData sp in searcherProperties)
+                {
+                    Console.WriteLine("\t{0}: {1}", sp.Name, sp.Value);
+                }
+            }
+            Environment.Exit(0);
+        }
+
+        /// <summary>
+        /// Generates a text file of the information of the computer specified
+        /// </summary>
+        /// <param name="root">The connection string to the computer to generate info for.</param>
+        /// <param name="fileName">The filename of the file to generate.</param>
+        public static void GenerateFullInfoToFile(string root, string fileName)
+        {
+            StringBuilder sb = new StringBuilder();
+            ManagementObject mo = new ManagementObject(root);
+            sb.AppendFormat("============================ SYSTEM PROPERTIES =========================\n\n");
+            foreach (PropertyData d in mo.SystemProperties)
+            {
+                sb.AppendFormat("{0} = {1}\n", d.Name, d.Value);
+            }
+            sb.AppendLine("\n================================ PROPERTIES ============================\n");
+            foreach (PropertyData d in mo.Properties)
+            {
+                if (d.Value is string[])
+                {
+                    sb.AppendFormat("{0} = \n", d.Name);
+                    foreach (string s in d.Value as string[])
+                    {
+                        sb.AppendFormat("\t{0}\n", s);
+                    }
+                } else {
+                    sb.AppendFormat("{0} = {1}\n", d.Name, d.Value);
+                }
+            }
+            System.IO.File.WriteAllText(fileName, sb.ToString());
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
+        public static void DisplayInfoFromFile(string fileName)
+        {
+
+            string[] input = System.IO.File.ReadAllLines(fileName);
+            foreach (string line in input)
+            {
+                Console.WriteLine("{0}", line);
+            }
+            Console.Read();
+        }
+
+        public static void DisplayInfo(string root)
+        {
+            ManagementObject mo = new ManagementObject(root);
+            Console.WriteLine("\n================================ PROPERTIES ============================\n");
+            foreach (PropertyData d in mo.Properties)
+            {
+                if (d.Value is string[])
+                {
+                    Console.WriteLine("{0} = \n", d.Name);
+                    foreach (string s in d.Value as string[])
+                    {
+                        Console.WriteLine("\t{0}\n", s);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("{0} = {1}\n", d.Name, d.Value);
+                }
+            }
+        }
+        
+        public static void GetCIMV2Classes()
+        {
+            //ArrayList objal = new ArrayList();
+            ConnectionOptions co = new ConnectionOptions();
+            // "", "Henry", "Password123", "", ImpersonationLevel.Impersonate, AuthenticationLevel.Connect, true, null, 10000);
+            ManagementScope scope = new ManagementScope(@"\\.\root\CIMV2", co);
+            ObjectQuery query = new ObjectQuery("SELECT * FROM meta_class where __class like \"%\"");
+
+            Console.WriteLine("Connected\n");
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+            ManagementObjectCollection collection = searcher.Get();
+
+            foreach (ManagementObject wmiClass in collection)
+            {
+                Console.WriteLine("Class : {0}\n", wmiClass.Path.ClassName);
+            }
+
+        }
+    }
+
+}
